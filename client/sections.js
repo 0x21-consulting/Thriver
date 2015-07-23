8da64f7b0e287efac78f3ce2328030bf4296dcac
Micah Henning <me@micahhenning.com>
2015-07-23 18:04:10 -0500

    Made new Work template dynamic.

--- client/sections.js
+++ client/sections.js
@@ -1,4 +1,24 @@
-var Sections = new Mongo.Collection('sections');
+var Sections = new Mongo.Collection('sections'),
+
+// Helper for changing tabs
+changeTabs = function (event) {
+    console.debug(event);
+    var parent = event.delegateTarget.parentElement.parentElement,
+        active = parent.querySelectorAll('.active'),
+        i = 0, j = active.length;
+        
+    console.debug('active', active);
+    // Remove active class from all elements under parent
+    for (; i < j; ++i)
+        active[i].classList.remove('active');
+    
+    // Make article with same ID as tab active
+    parent.querySelector('[data-id="' + event.target.dataset.id + '"]').
+        classList.add('active');
+        
+    // Make tab active, too
+    event.target.classList.add('active');
+};
 
 // Dynamically generate anchor name
 Template.registerHelper('anchor', function (name) {
@@ -42,3 +62,44 @@ Template.header.helpers({
         return Sections.find({ displayOnPage: true, name: { $ne: null } });
     }
 }); 
+
+// Tabs
+Template.tab.helpers({
+    name: function (id) {
+        return Sections.findOne({ '_id': id }, { '_id': 0, name: 1 }).name;
+    },
+    icon: function (id) {
+        return Sections.findOne({ '_id': id }, { '_id': 0, icon: 1 }).icon;
+    }
+});
+
+// Articles
+Template.article.helpers({
+    content: function (id) {
+        return Sections.findOne({ '_id': id }, { '_id': 0, content: 1 }).content;
+    },
+    icon: function (id) {
+        return Sections.findOne({ '_id': id }, { '_id': 0, icon: 1 }).icon;
+    },
+    name: function (id) {
+        return Sections.findOne({ '_id': id }, { '_id': 0, name: 1 }).name;
+    }
+});
+// Set the first tab as active
+Template.tab.onRendered(function () {
+    var parent;
+    
+    try {
+        // TODO:  How robust is this?!
+        parent = this.firstNode.parentElement.parentElement.parentElement;
+        
+        // Set the very first result as active.  Should be the first in the DOM.
+        parent.querySelector('main > article').classList.add('active');
+        parent.querySelector('menu li').classList.add('active');
+    } catch (error) { /* no recovery */ }
+});
+
+// Helper for changing tabs
+Template.tab.events({
+    'click li': changeTabs
+});
