2479fed2a9e3b82cd5692ae51c54e59714566b6a
eoghan <eoghan.tadhg@gmail.com>
2015-08-03 19:06:02 -0500

    Massive overhaul of file structure and template relationships within Client folder. Template files and LESS file style require organization, however file structure should be more sound.

---
+++ client/lib/js/jQueryHelpers.js
@@ -0,0 +1,62 @@
+/* -All jQuery & JS usage for UI enhancements has been moved from templates
+    to this document for review, improvements and to be moved as seen fit. */
+
+
+Template.body.onRendered(function () {
+    //Bind the 'esc' button to close any active 'modal' states
+    //Scroll event which toggles between header states
+    //Animate elements as they load in
+
+
+    //Header State Change (window.scroll)
+    window.addEventListener("scroll",function() { 
+        if(window.scrollY > 160) {
+            $('body').addClass('scrolled');
+        }
+        else {
+            $('body').removeClass('scrolled');
+            $('header nav').removeClass('active');
+        }
+    },false);
+
+
+
+    //Menu Toggle Click
+    $('.menu-toggle').click(function(){
+        $('body').toggleClass('open-nav');
+    });
+
+
+
+    //Carousel Function
+    $('.carousel nav li').click(function(){
+        var left = parseInt($('.carousel main').css('left'));
+        var carouselWidth = $('.carousel main').width();
+        if($(this).hasClass('prev')){
+            if(left == 0 ){
+                // do nothing
+            }
+            else { 
+                var prevLeft = left + carouselWidth;
+                $('.carousel main').css({left: prevLeft});
+            }
+        }
+        if($(this).hasClass('next')){
+            if(left == - $('.carousel main article').length * carouselWidth + carouselWidth){
+                // do nothing
+            }
+            else { 
+                var nextLeft = left - carouselWidth;
+                $('.carousel main').css({left: nextLeft});
+            }
+        }
+    });
+    $('.details-ref').click(function(){
+        $('.carousel').addClass('active');
+     });
+    $('.close-carousel').click(function(){
+        $('.carousel').removeClass('active');
+     });
+
+
+}); //End jQuery Helpers
