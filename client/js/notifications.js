60bf5181b634c82c75a4519d1bda56ea79c159db
Micah Henning <me@micahhenning.com>
2015-09-10 13:07:17 -0500

    Added twitter stream.  Client notifications display, including count.  Added twitter meteor libraries, but may not be necessary. still trying to figure out how I can bind to twitter feed updates to "notify" user as new tweets come in.

---
+++ client/js/notifications.js
@@ -0,0 +1,39 @@
+// Subscribe to notifications
+Meteor.subscribe('notifications');
+
+// Create collection of notifications subscribed
+var notifications = new Mongo.Collection('notifications');
+
+// Notification helpers
+Template.utilityNav.helpers({
+    // Show notification count
+    count: function () {
+        var all = notifications.find({});
+        // Don't return 0
+        return all.count() ? all.count() : '';
+    },
+    // Return all notifications
+    notifications: function () {
+        var all = notifications.find({});
+        
+        // If no notifications, say so
+        if (! all.count())
+            return [{ template: 'notificationEmpty' }];
+        else
+            return all;
+    }
+});
+
+// Debug controls
+Template.debug.events({
+    // Add notification
+    'click #dAddNotification button': function (event) {
+        event.stopPropagation(); event.preventDefault();
+        
+        var parent = event.target.parentElement;
+        Meteor.call('addNotification',
+            parent.querySelector(':nth-child(1)').value,
+            parent.querySelector(':nth-child(2)').value,
+            parent.querySelector(':nth-child(3)').value);
+    }
+});
--- server/notifications.js
+++ server/notifications.js
@@ -2,21 +2,51 @@
 var notifications = new Mongo.Collection('notifications');
 
 // Structure
-//   _id         {int}      auto_incr
-//   type        {int}
+//   _id         {string}      auto_incr
+//   template    {string}
 //   text        {string}
 //   userId      {string}
 
-// Structure @type
-//   0           approval
-//   1           renewal notice
+// @values template
+//   templateGeneric        generic
+//   templateApproval       access control request
+//   templateRenewal        renewal notice
 
 // Publish notifications
 Meteor.publish('notifications', function () {
     // If the user is logged in
     if (this.userId) {
-        notifications.find({ userId: this.userId });
+        return notifications.find({ userId: this.userId });
     } else
         // Not logged in
         return [];
+});
+
+// Notification-related events
+Meteor.methods({
+    /** 
+     * Add a notification for a given user
+     * @method
+     *   @param {string} userId   - the ID of the user to whom to push the notifcation
+     *   @param {int}    template - the type of notification to display
+     *   @param {string} text     - the message to display in the notification
+     */
+    addNotification: function (userId, template, text) {
+        // Check Authorization
+        // (user must be logged in)
+        if (!Meteor.userId())
+            throw new Meteor.Error('not-authorized');
+            
+        // Mutual Suspicion
+        userId    = '' + userId;
+        template  = '' + template;
+        text      = '' + text;
+        
+        // Add notification
+        notifications.insert({
+            userId  : userId,
+            template: template,
+            text    : text
+        });
+    }
 });
