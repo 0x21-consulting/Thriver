841935c9bb0994b76c7f19877968b528336491a9
eoghan <eoghan.tadhg@gmail.com>
2016-02-29 05:03:39 -0800

    Styles for active work navigation complete.

--- client/modules/work/work.js
+++ client/modules/work/work.js
@@ -1,9 +1,25 @@
 Template.utility.onRendered(function () {
     //Should be attributed to correct alert (Figured alerts would be managed via a meteor variable)
-    $('.workNav li a').click(function(){
+    $('.workNav > ul > li > a, .workNav > ul > li > .icon > a').click(function(){
         event.preventDefault();
+        $('.workNav >ul > li').removeClass('active');
+        $(this).closest('li').addClass('active');
         if(!$('body').hasClass('workActive')){
             $('body').addClass('workActive');
+        }
+    });
+
+    $('.workNav ul > li > ul > li > a').click(function(){
+        event.preventDefault();
+        $('.workNav ul > li > ul > li').removeClass('active');
+        $(this).parent().addClass('active');
+        $(this).parent().parent().parent().addClass('active');
+    });
+
+    $('.workNav li.backToIndexWork a').click(function(){
+        event.preventDefault();
+        if($('body').hasClass('workActive')){
+            $('body').removeClass('workActive');
         }
     });
 });
