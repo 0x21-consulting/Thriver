2471edea711420e16a175e444f66487cfda3380e
eoghan <eoghan.tadhg@gmail.com>
2016-03-06 23:56:11 -0800

    Modified 'Outreach' section and replaced image. (Work in progress)

--- client/modules/outreach/outreach.js
+++ client/modules/outreach/outreach.js
@@ -1,16 +1,27 @@
 Template.outreach.events({
     // Switch tabs
     'click ul.outreachTabs > li': function (event) {
-        var index = $(event.target).index() + 1;
-        
-        // Set the active tab
+        /*var index = $(event.target).index() + 1;
         $('ul.outreachTabs > li').removeClass('active');
         $(event.target).addClass('active');
-        
-        // Set the active content
-        $('ul.outreachContent').addClass('active');
-        $('ul.outreachContent > li').removeClass('active');
-        $('ul.outreachContent > li:nth-child(' + index + ')').addClass('active');
+        //Transition
+        $("body").addClass("outreachTransition").delay(500).queue(function(){
+            $(this).removeClass("outreachTransition").dequeue();
+            // Set the active content
+            $('ul.outreachContent').addClass('active');
+            $('ul.outreachContent > li').removeClass('active');
+            $('ul.outreachContent > li:nth-child(' + index + ')').addClass('active');
+        }); */    
+
+        var index = $(event.target).index() + 1;
+        $('ul.outreachTabs > li').removeClass('active');
+        $(event.target).addClass('active');
+        $("body").addClass("outreachTransition").delay(200).queue(function(){
+            $(this).removeClass("outreachTransition").dequeue();
+            $('ul.outreachContent').addClass('active');
+            $('ul.outreachContent > li').removeClass('active');
+            $('ul.outreachContent > li:nth-child(' + index + ')').addClass('active');
+        });
     }
 
 });
