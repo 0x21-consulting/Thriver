8481027b56cf932c9232e2bc7712c60cf7800758
Micah Henning <me@micahhenning.com>
2016-03-06 20:31:03 -0600

    Wrote account creation functionality. Connected profile attributes to account settings on profile tab. Wrote account settings update functionality (so far only name can be updated).  Wrote email account verification system. Once an email address is verified, it is automatically paired with an organization if the domain name matches.

--- client/modules/account/account.js
+++ client/modules/account/account.js
@@ -1,5 +1,6 @@
 // Global on purpose
 lastLogin = new ReactiveVar(new Date());
+organization = new ReactiveVar('');
 
 /**
  * Determine last login and set to Reactive var
@@ -10,11 +11,43 @@ var getLastLogin = function () {
         // Update reactive var
         lastLogin.set(result);
     });
+},
+
+/**
+ * Get the user's assigned organization, if they have one
+ * @method
+ */
+getOrganization = function () {
+    Meteor.call('getOrganization', function (error, result) {
+        // Update reactive var
+        organization.set(result);
+    });
 };
 
 // Bind to login and on load
 Template.body.onCreated(getLastLogin);
 Accounts.onLogin       (getLastLogin);
+Template.body.onCreated(getOrganization);
+Accounts.onLogin       (getOrganization);
+
+/**
+ * Assign a user's organization upon account creation
+ * @method
+ *   @param {string}   token - The email verification token
+ *   @param {Function} done  - Callback once verification flow is complete
+ */
+Accounts.onEmailVerificationLink(function (token, done) {
+    // Verify email
+    Accounts.verifyEmail(token, function (error) {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        
+        // Assign organization
+        Meteor.call('assignOrganization', Meteor.userId(), done);
+    });
+});
 
 function removeActiveClass(){
     $('.accountDetailsContent > li').removeClass('active');
@@ -27,16 +60,91 @@ function removeOpenAccounts(){
 
 //Register Form
 Template.register.events({
-    'submit form': function(event){
-        event.preventDefault();
-        var nameVar = event.target.registerName.value;
-        var providerVar = event.target.registerProvider.value;
-        var emailVar = event.target.registerEmail.value;
-        var passwordVar = event.target.registerPassword.value;
-        console.log("Form submitted.");
+    /**
+     * Register a new account
+     * @method
+     *   @param {$.Event} - Form submission event
+     */
+    'submit form': function (event) {
+        if (! (event instanceof $.Event))
+            return;
+        
+        // Prevent navigation
+        event.preventDefault(); event.stopPropagation();
+        
+        // Get form values
+        var name     = event.target.name.value,
+            email    = event.target.email.value,
+            password = event.target.password.value,
+        
+        // Handle login errors    
+        handleError = function (message) {
+            var error = event.target.querySelector('#register-error');
+            
+            if (error instanceof Element)
+                error.textContent = message;
+        };
+        
+        // Enforce proper name format by removing excess spaces,
+        // making all lower case, then capitalizing just the first character
+        name = name.trim().replace(/\s+/g, ' ').toLowerCase().split(/\s/);
+        for (i = 0, j = name.length; i < j; ++i)
+            name[i] = name[i].charAt(0).toUpperCase() + name[i].substr(1);
+        name = name.join(' ');
+        
+        Accounts.createUser({
+            email: email,
+            password: password,
+            profile: {
+                firstname: name.replace(/^(.+)\s.+/, '$1'),
+                lastname : name.replace(/^.+\s(.+)/, '$1')
+            }
+        },
+        /**
+         * Handle Account creation callback
+         * @method
+         *   @param {Error} error - Option argument passed on error
+         */
+        function (error) {
+            if (error) {
+                handleError(error);
+                return;
+            }
+            
+            // Send email verification email
+            Meteor.call('sendVerificationEmail', Meteor.userId());
+            
+            // Notify user to check email
+            document.querySelector('section.register form').classList.add('hide');
+            document.querySelector('section.register p').classList.remove('hide');
+        });
+        
     },
-    'click section.register .showLogin': function (event) {
-        document.body.classList.remove('registerMob');
+    /**
+     * Ensure password is the same in both fields
+     * @method
+     *   @param {$.Event} event - Event received from keyup event
+     */
+    'keyup [name="repeat"]': function (event) {
+        if (! (event instanceof $.Event) )
+            return;
+        
+        var parent   = event.target.parentElement,
+            password = parent.parentElement['password'];
+        
+        if (password instanceof Element)
+            if (password.value === event.target.value) {
+                // Let user know passwords match
+                parent.classList.remove('noMatch');
+                // Allow form submit now
+                parent.parentElement['submitButton'].disabled = false;
+                return;
+            }
+        
+        // By default indicate there's no match
+        parent.classList.add('noMatch');
+        // And prevent the form from submitting until they do match
+        parent.parentElement['submitButton'].disabled = true;
     }
 });
 //Login Form
@@ -50,6 +158,9 @@ Template.login.events({
         // Handle login errors    
         handleError = function (message) {
             var error = event.target.querySelector('#login-error');
+            
+            // Show error element
+            error.classList.remove('hide');
             
             if (error instanceof Element)
                 error.textContent = message;
@@ -71,8 +182,8 @@ Template.login.events({
             // Handle errors
             switch (error.error) {
                 case 403:
-                    handleError('So sorry. Either you\'ve mistyped your email address, \
-                        or your password is incorrect.');
+                    handleError('So sorry. Either you\'ve mistyped your email address,' + 
+                        ' or your password is incorrect.');
                     break;
                 default:
                     handleError('An unknown error has occurred.');
@@ -161,13 +272,73 @@ Template.accountDetails.events({
 
 });
 
+// Full name in Utility bar
 Template.utility.helpers({
     name: function () {
         var user = Meteor.user();
         if (user && user.profile)
             return user.profile.firstname + ' ' + user.profile.lastname;
-        else
-            return '';
+        return '';
+    }
+});
+
+// Populate Profile tab under Account Overview
+Template.profile.helpers({
+    name: function () {
+        var user = Meteor.user();
+        if (user && user.profile)
+            return user.profile.firstname + ' ' + user.profile.lastname;
+        return '';
+    },
+    organization: function () {
+        return organization.get();
+    },
+    email: function () {
+        var user = Meteor.user();
+        if (user && user.emails && user.emails[0])
+            return user.emails[0].address;
+        return '';
+    }
+});
+// Update Profile
+Template.profile.events({
+    /**
+     * Handle submission of Account Settings form under Profile tab
+     * @method
+     *   @param {$.Event} event - Event passed by form submission
+     */
+    'submit form': function (event) {
+        if (! (event instanceof $.Event))
+            return;
+        
+        // Prevent navigation
+        event.preventDefault(); event.stopPropagation();
+        
+        var name  = event.target[0].value,
+            email = event.target[2].value,
+            user  = Meteor.user(), i, j;
+        
+        // Can't do anything if not logged in
+        if (!user || !user.profile) return;
+        
+        // Enforce proper name format by removing excess spaces,
+        // making all lower case, then capitalizing just the first character
+        name = name.trim().replace(/\s+/g, ' ').toLowerCase().split(/\s/);
+        for (i = 0, j = name.length; i < j; ++i)
+            name[i] = name[i].charAt(0).toUpperCase() + name[i].substr(1);
+        name = name.join(' ');
+        
+        // Compare with db
+        if (name !== ( user.profile.firstname + ' ' + user.profile.lastname )) {
+            // Update profile (clients are allowed profile changes)
+            Meteor.users.update({
+                _id: Meteor.userId(),
+            }, { $set: { 
+                'profile.firstname': name.replace(/^(.+)\s.+/, '$1'),
+                'profile.lastname' : name.replace(/^.+\s(.+)/, '$1')
+            }});
+        }
+        
     }
 });
 
---
+++ server/accounts.js
@@ -0,0 +1,101 @@
+/**
+ * The Organizations collection
+ */
+Organizations = new Mongo.Collection('organizations');
+
+Meteor.methods({
+    /**
+     * Handle sending email verification email
+     * @method
+     *   @param {String} id - Meteor user ID
+     */
+    sendVerificationEmail: function (id) {
+        // Mutual suspicion
+        id = '' + id;
+        
+        // Send email
+        Accounts.sendVerificationEmail(id);
+    },
+    /**
+     * Assign organizational association
+     * @description
+     *   A user's organizational association is determined by
+     *   their email domain.  Therefore, only once a user has
+     *   established an account and verified their email address
+     *   will the association be established.
+     * @method
+     *   @param {string}   id       - The user's Meteor ID
+     *   @param {Function} callback
+     */
+    assignOrganization: function (id, callback) {
+        // Mutual suspician
+        id = '' + id;
+        
+        // Get user based on verification token
+        var matches, user = Meteor.users.find({ '_id': id }, { 
+            // Only get user id and emails array
+            'emails': 1 }).fetch();
+        
+        // Validate user
+        user = user[0]; // break out of array
+        if (!user || !(user.emails instanceof Array) )
+            return;
+        
+        // Get organization based on user's email domain
+        matches = Organizations.find({
+            domain: user.emails[0].address.replace(/.+@(.+)/, '$1')
+        }).fetch();
+        
+        // If there's a match, associate
+        if (matches instanceof Array)
+            if (matches[0]._id)
+                Meteor.users.update({ _id: user._id }, { 
+                    $set : { organization: matches[0]._id }});
+        
+        // Otherwise do nothing
+        
+        // Execute callback
+        if (callback instanceof Function)
+            callback();
+    },
+    /**
+     * Return the name of the user's designated organization association
+     * @function
+     * @returns {string}
+     */
+    getOrganization: function () {
+        // Nothing to do if no user is logged in, or if they
+        // don't have a designated organization
+        if (!Meteor.user() || !Meteor.user().organization)
+            return '';
+            
+        var organization = Organizations.find({ _id: Meteor.user().organization }).fetch();
+        
+        organization = organization[0]; // break out of array
+        
+        if (organization && organization.name)
+            return organization.name;
+        
+        // User has an organization but it's not found in the database
+        return '';
+    }
+});
+
+/**
+ * Email Template details
+ */
+Accounts.emailTemplates.siteName = 'WCASA';
+Accounts.emailTemplates.from     = 'WCASA <noreply@wcasa.org>';
+
+/**
+ * Verify Email details
+ */
+Accounts.emailTemplates.verifyEmail.subject = function (user) {
+    return 'Verify your email address for WCASA';
+};
+Accounts.emailTemplates.verifyEmail.text = function (user, url) {
+    return 'Hello ' + user.profile.firstname + '!\n\n' +
+        'To verify your account email, simply click the link below.\n\n' + 
+        url + '\n\n' + 'If you weren\'t expecting this email, simply delete it.\n\n' + 
+        'Thanks!\n\nAll of us at WCASA';
+};
