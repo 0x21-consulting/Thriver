9eec2aa3c349b16431c3f485b7731ead2bd81a3f
eoghan <eoghan.tadhg@gmail.com>
2016-04-20 03:57:10 -0700

    Completed states for library list items.

--- client/modules/resources/resources.js
+++ client/modules/resources/resources.js
@@ -15,6 +15,18 @@ Template.resources.events({
         // Set the active content
         $('ul.resourcesContent > li').removeClass('active');
         $('ul.resourcesContent > li:nth-child(' + index + ')').addClass('active');
+    },
+    'click .loginToRequest > button': function (event) {
+        if (window.innerWidth >= 768) {
+            $('.overlay').click();
+            $('li.login').click();
+        } 
+        /*else{
+            removeOpenAccounts();
+            $('.mobileOverlay').click();
+            $('.menuToggle').click();
+            $('.mobileMenu li.donate').click();
+        }*/
     }
 });
 
