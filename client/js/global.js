155a3a23c8ff391b7ef52d075282c76d240cfdd4
eoghan <eoghan.tadhg@gmail.com>
2016-05-07 22:57:46 -0700

    Added different data-types to utility sidebar li.

--- client/js/global.js
+++ client/js/global.js
@@ -2,12 +2,12 @@
 //Off Canvas Effects
 function toggleOffCanvas() {
     // Canvas Variables
-    var eachToggle = document.querySelectorAll('.utilityNav > ul > li[data-sidebar]');
-    var canvas = document.getElementById('canvas');
+    var eachToggle = document.querySelectorAll('.utilityNav > ul > li[data-width]');
     var overlay = document.getElementById('overlay');
-
-    //Close Canvas Elements if Overlay or active LI is clicked
     var buttonParent = event.target.parentNode;
+    var thisSidebarWidth = buttonParent.dataset.width;
+
+    //Close open canvas elements if overlay or active li is clicked
     if(buttonParent.classList.contains('active') || event.target == overlay){
         for (var i = 0, element; element = eachToggle[i]; i++) {
             element.classList.remove('active');
@@ -15,18 +15,25 @@ function toggleOffCanvas() {
         document.body.classList.remove('offCanvasActive');
     }
     // Open Overlay and offCanvas elements if clicking inactive LI
-    else if(buttonParent.hasAttribute('data-sidebar') && buttonParent.classList !== 'active'){
+    else if(buttonParent.hasAttribute('data-width') && buttonParent.classList !== 'active'){
         for (var i = 0, element; element = eachToggle[i]; i++) {
             element.classList.remove('active');
         }
         buttonParent.classList.add('active');
-        document.body.classList.add('offCanvasActive');        
+        document.body.classList.add('offCanvasActive');    
+        if(buttonParent.dataset.position == 'left'){
+            document.body.classList.add('offCanvasLeft');  
+        }
+        if(buttonParent.dataset.position == 'right'){
+            document.body.classList.add('offCanvasRight');  
+        }
+        document.body.classList.add('offCanvasWidth' + thisSidebarWidth);       
     }
 }
 
 Template.body.events({
     //Canvas Actions
-    'click .utilityNav > ul > li[data-sidebar] button': function (event) {
+    'click .utilityNav > ul > li[data-width] button': function (event) {
         toggleOffCanvas();
     },
     'click .overlay': function (event) {
