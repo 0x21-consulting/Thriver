d3f6d2a5ce4414fc4aefe4fa7746e81dc4efba99
eoghan <eoghan.tadhg@gmail.com>
2016-05-13 19:55:01 -0700

    Added content array item to sidebar.

--- client/js/content.js
+++ client/js/content.js
@@ -1,17 +1,54 @@
 //This file contains temporary content declarations to eventually be merged into Mongo
 
 //Sidebar elements
-var sidebars = [
-    { id: 'accounts', width: 231, position:'left' },
-    { id: 'notifications', width: 231, position:'left' },
-    { id: 'signin', width: 231, position:'left' },
-    { id: 'register', width: 231, position:'left' },
-    { id: 'twitter', width: 231, position:'right' },
-    { id: 'resources', width: 231, position:'right' },
-    { id: 'news', width: 231, position:'right' },
-    { id: 'donate', width: 231, position:'right' },
-    { id: 'lang', width: 231, position:'right' }
+var sidebars = [{ 
+        id: 'accounts', 
+        width: 231, 
+        position:'left',
+        content: 'acc'
+    },{ 
+        id: 'notifications', 
+        width: 231, 
+        position:'left',
+        content: 'notif' 
+    },{ 
+        id: 'signin', 
+        width: 231, 
+        position:'left',
+        content: 'signin' 
+    },{ 
+        id: 'register', 
+        width: 231, 
+        position:'left',
+        content: 'register' 
+    },{ 
+        id: 'twitter', 
+        width: 231, 
+        position:'right',
+        content: 'twitter' 
+    },{ 
+        id: 'resources', 
+        width: 231, 
+        position:'right',
+        content: 'resources' 
+    },{ 
+        id: 'news', 
+        width: 231, 
+        position:'right',
+        content: 'news' 
+    },{ 
+        id: 'donate', 
+        width: 231, 
+        position:'right',
+        content: 'donate' 
+    },{ 
+        id: 'lang', 
+        width: 231, 
+        position:'right',
+        content: 'lang' 
+    }
 ];
+
 Template.sidebars.section = function() {
     return sidebars;
 };
