ce16d356e048360ef81492520b658cdb60946d0f
eoghan <eoghan.tadhg@gmail.com>
2016-05-15 20:08:48 -0700

    Refined toggleCanvas events to datasets rather than classes. Further work on toggle Groups.

--- client/js/global.js
+++ client/js/global.js
@@ -6,31 +6,49 @@ Template.registerHelper('equals', function (a, b) {
 
 //UI Helpers
 //Add Class
-function addClass(el, cls){
-    el.classList.add(cls);
+function addClass(e, cls){
+    e.classList.add(cls);
 }
 //Remove Class
-function removeClass(el, cls){
-    el.classList.remove(cls);
+function removeClass(e, cls){
+    e.classList.remove(cls);
 }
 
-function activeSwitch(el, state){
-    el.dataset.active = state;
-}
+
 //Remove Class (by Prefix)
-function removeClassByPrefix(el, prefix) {
+function removeClassByPrefix(e, prefix) {
     var regx = new RegExp('\\b' + prefix + '.*?\\b', 'g');
-    el.className = el.className.replace(regx, '');
-    return el;
+    e.className = e.className.replace(regx, '');
+    return e;
 }
 
+
+function hidden(e,state){
+    if (state == false){
+        e.setAttribute('aria-hidden', 'false');
+        e.dataset.hidden = 'false';
+    } else{
+        e.setAttribute('aria-hidden', 'true');
+        e.dataset.hidden = 'true';       
+    }
+}
+
+function active(e,state){
+    if(state == true){
+        e.dataset.active = 'true';
+        e.setAttribute('aria-expanded','true');
+    } else{
+        e.dataset.active = 'false';
+        e.setAttribute('aria-expanded','false');        
+    }
+}
 
 
 
 
 //Focus Trigger Functions
 //Needs written work, however function events occur flawlessly
-var resetFocus = false;
+/*var resetFocus = false;
 var startGlobalFocus = document.getElementById('focus0');
 var endGlobalFocus = document.getElementById('focus0end');
 $("body").on("keydown", function(event) {
@@ -60,57 +78,100 @@ $("body").on("keydown", function(event) {
             resetFocus = true;
         }
     }
+});*/
+
+/*
+focusableEls = 'a[href], area[href], input, select, textarea, button, iframe, object, embed, *[tabindex], *[contenteditable]';
+nonFocusableEls '[tabindex=-1], [disabled], :hidden';
+focusable = $(focusableEls:not(nonFocusableEls));
+focusGroup= $()
+
+focusGroups = querySelectorAll('[data-focus]');
+for (var i = 0, e; e = focusGroups[i]; i++) {
+    e.querySelectorAll(this.id + " > .foo");
+
+for (var i = 0, e; e = focusable[i]; i++) { 
+//Focus Trigger Functions
+//The selector, ":focusable" is derivative of the library: focusable.js
+var resetFocus = false;
+var focusToGroup = false;
+$("body").on("keydown", function(event) {
+    if(event.keyCode == 9){
+        var active;
+        active = document.activeElement; 
+        if(resetFocus == true){
+            $("[data-focus=global] :focusable:first-of-type").focus();
+            resetFocus = false;
+            event.preventDefault();
+        } 
+        if(focusToGroup == true){
+            $("[data-focus=group][data-active=true] :focusable:first-of-type").focus();
+            focusToGroup = false;
+            event.preventDefault();
+        }
+        if(active == $("[data-focus=global] :focusable:last-of-type")){ focusToGroup = true; }
+        if(active == $("[data-focus=group] :focusable:last-of-type")){ resetFocus = true; }
+    }
 });
 
 
-//Functions
+
+        var sidebarSections = document.querySelectorAll('section.sidebar');
+        for (var i = 0, e; e = sidebarSections[i]; i++) { 
+            if (e.dataset.active == 'true'){
+                $('#' + e.id + " [data-focus=group] :focusable:first-of-type").focus();
+                event.preventDefault();
+            }
+        }
+
+*/
+
+function clearCanvas(){
+    document.body.classList.remove('noScroll');
+    var canvas = document.getElementById('canvas');
+    canvas.setAttribute('data-canvas-position','');
+    canvas.setAttribute('data-canvas-state','closed');
+    canvas.setAttribute('data-canvas-width','');
+}
+
 //Off Canvas Effects
 //event.target function fires on click events of any element containing the data-attribute, "data-sidebar".
-//Note: Focus actions may be too expensive. will need to evaluate.
 function toggleCanvas() {
 
     // Canvas Variables
     var toggle = document.querySelectorAll('[data-sidebar]');
     var overlay = document.getElementById('overlay');
     var sidebar = document.querySelectorAll('section.sidebar');
-    var body = document.body;
+    var canvas = document.getElementById('canvas');
 
-    //Deactivate any open focusTraps
-    //focusTrap.deactivate();
-
-    //Close open canvas elements if overlay or active li is clicked
-    if(event.target.dataset.active == 'true' || event.target == overlay || event.target.classList.contains('closeTab')){
-        for (var i = 0, e; e = toggle[i]; i++) { activeSwitch(e,'false'); } //Clear all active Toggles
-        for (var i = 0, e; e = sidebar[i]; i++) { activeSwitch(e,'false'); } //Clear all active Sidebars
-        removeClassByPrefix(body, "canvas"); //Remove all canvas effect classes
-        overlay.setAttribute('aria-hidden', 'true'); //toggle aria-hidden
-        document.getElementById('focus0').focus();
+    //Close open canvas elements if overlay or active li is clicked. Or if close canvas event fired
+    if(event.target.dataset.active == 'true' || event.target == overlay || event.target.getAttribute('data-canvas-event') == 'close'){
+        for (var i = 0, e; e = toggle[i]; i++) { active(e,false); } //Clear all active Toggles
+        for (var i = 0, e; e = sidebar[i]; i++) { active(e,false); } //Clear all active Sidebars
+        clearCanvas(); //Remove all canvas effect classes
+        hidden(overlay,true);
+        //NEED TO FOCUS: document.getElementById('focus0').focus();
     }
 
     // Open Overlay and offCanvas elements if clicking inactive list item
     else if(event.target.hasAttribute('data-sidebar') && event.target.dataset.active == 'false'){
-        removeClassByPrefix(body, 'canvasW'); //Remove any currently active sidebar-width effect classes
-        addClass(body,'canvasActive'); //Add master canvas effect class
-        overlay.setAttribute('aria-hidden', 'false'); //toggle aria-hidden
-        for (var i = 0, e; e = toggle[i]; i++) { activeSwitch(e,'false'); } //Clear all active toggles
-        activeSwitch(event.target,'true'); //Add active class to clicked element
+        clearCanvas();
+        document.body.classList.add('noScroll');
+        canvas.setAttribute('data-canvas-state','open'); //Add master canvas effect class
+        hidden(overlay,false);
+        for (var i = 0, e; e = toggle[i]; i++) { active(e,false); } //Clear all active toggles
+        active(event.target,true); //Add active class to clicked element
         //Sets values based on the parameters of the current sidebar
         for (var i = 0, e; e = sidebar[i]; i++) {
-            activeSwitch(e,'false');; //Clear all active sidebars
+            active(e,false);; //Clear all active sidebars
             if (e.getAttribute('id') == event.target.dataset.sidebar){ //If Sidebar ID matches toggles' data-sidebar
-                activeSwitch(e,'true'); //Add active class to given sidebar
+                active(e,true); //Add active class to given sidebar
                 //focusTrap.activate(e); //Pauses Focus to Group
-                addClass(body,'canvasW' + e.dataset.width); //Add new sidebar-width effect class 
-                if(e.dataset.position == 'left'){ // Add/Remove left/right
-                    removeClass(body,'canvasRight');
-                    addClass(body,'canvasLeft');
-                }
-                if(e.dataset.position == 'right'){ // Add/Remove right/left
-                    body.classList.remove('canvasLeft'); 
-                    body.classList.add('canvasRight');  
-                }
+                canvas.setAttribute('data-canvas-width',e.dataset.width); //Add new sidebar-width effect class 
+                if(e.dataset.position == 'left'){ canvas.setAttribute('data-canvas-position','left'); }
+                if(e.dataset.position == 'right'){ canvas.setAttribute('data-canvas-position','right'); }
             }
-            $('#' + e.id + " [data-focus=head]").focus();
+            //NEED TO FOCUS: $('#' + e.id + " [data-focus=head]").focus();
             event.preventDefault();
         }
     }
