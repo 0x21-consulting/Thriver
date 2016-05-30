c72e18fa21ca06e53600da091b5e65a753a56ec8
EoghanTadhg <eoghan.tadhg@gmail.com>
2016-05-29 22:16:59 -0700

    Organized content files.

---
+++ client/content/aside.content.js
@@ -0,0 +1,114 @@
+//Sidebar sections
+var sidebars = [{
+        title: 'Account Details',
+        id: 'accounts', //Sets the ID of the sidebar which gets targeted by utility nav items 
+        width: 656, //Sets the sidebar width & body class
+        position:'left', //Which Direction the sidebar appears from
+        tabs: [{ //If sidebar has tabs: use this property
+                title: 'Profile',
+                icon : 'user',
+                id: 'profile',
+                template : 'profile'
+            },{
+                title: 'Subscriptions',
+                icon : 'envelope',
+                id: 'subscriptions',
+                template : 'subscriptions'
+            },{
+                title: 'Requests',
+                icon : 'book',
+                id: 'requests',
+                template: 'requests'
+            }
+        ]
+    },{ 
+        title: 'Notifications',
+        id: 'notifications', 
+        width: 356, 
+        position:'left',
+        template: 'notifications'
+    },{ 
+        title: 'Account Sign in',
+        id: 'login', 
+        width: 356, 
+        position:'left',
+        template: 'login'
+    },{ 
+        title: 'Create an Account',
+        id: 'register', 
+        width: 356, 
+        position:'left',
+        template: 'register' 
+    },{ 
+        title: 'Twitter',
+        id: 'twitter', 
+        width: 356, 
+        position:'right',
+        template: 'twitter' 
+    },{ 
+        title: 'Learning Center',
+        id: 'resources', 
+        width: 700, 
+        position:'right',
+        tabs: [{ //If sidebar has tabs: use this property
+                title: 'Library',
+                id: 'library',
+                template : 'library'
+            },{
+                title: 'Infosheets',
+                id: 'infosheets',
+                template : 'infosheets'
+            },{
+                title: 'Stats & Data',
+                id: 'stats-data',
+                template: 'statsData'
+            },{
+                title: 'Webinars',
+                id: 'webinars',
+                template: 'webinars'
+            }
+        ]
+    },{ 
+        title: 'Newsroom',
+        id: 'news', 
+        width: 1100, 
+        position:'right',
+        tabs: [{ //If sidebar has tabs: use this property
+                title: 'In the News',
+                id: 'in-the-news',
+                template : 'inTheNews'
+            },{
+                title: 'Press Releases',
+                id: 'press-releases',
+                template : 'pressReleases'
+            },{
+                title: 'Newsletters',
+                id: 'newsletters',
+                template: 'newsletters'
+            },{
+                title: 'Annual Reports',
+                id: 'annual-reports',
+                template: 'annualReports'
+            },{
+                title: 'Press & Media Kits',
+                id: 'press-media-kits',
+                template: 'pressMediaKits'
+            }
+        ]
+    },{ 
+        title: 'Donate',
+        id: 'donate', 
+        width: 656, 
+        position:'right',
+        template: 'donate' 
+    },{ 
+        title: 'Language Select',
+        id: 'lang', 
+        width: 356, 
+        position:'right',
+        template: 'languageSelect' 
+    }
+];
+Template.aside.item = function() {
+    return sidebars;
+};
---
+++ client/content/list.content.js
@@ -0,0 +1,50 @@
+var lists =[{
+    type: 'generic', //accepts: generic, details
+    itemType: 'category',
+    paginate: true, //Default is false
+    perPage: 10, //if paginate:true, how many before paginate
+    style: 'striped',
+    //If standard
+    items: {
+        title: 'One Webinar',
+        date: '06991020', //This is temporary
+        friendlyDate: '02/11/29',
+        content: 'lorem ipsum.'        
+    },
+    //If category
+    category: {
+        title: 'Summer Webinars',
+        id: 'summerWebinars',
+        items: {
+            title: 'One Webinar',
+            date: '06991020', //This is temporary
+            friendlyDate: '02/11/29',
+            content: 'lorem ipsum.'
+        }
+    },
+    //If toggle
+    toggle: {
+        title: 'Press Releases',
+        id: 'pressReleases',
+        style: 'striped'
+    },
+    //If catalog
+    catalog: {
+        title: 'There is diversity Within Diversity: Community Leaders Views on increasing diversity in youth serving organizations.',
+        id: 1,
+        byline: 'Sumru Erkut, et al., Center for Research on Women, 1993',
+        id: 'summerWebinars',
+        callNumber: '6504S',
+        copies: 1,
+        subjectHeadings: 'Ethnology -- United States. Minority teenagers -- United States -- Societies and clubs.',
+        classification: 'Organizational',
+        category: 'Organizational Material',
+        type: 'Book or Booklet',
+        status: {
+            type: 'transit',
+            style: 'neutral',
+            text: 'In Transit',
+
+        }
+    }
+}];
