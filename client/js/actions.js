f7ba06fe56819f3be9efe7e8e17b9113133781f0
EoghanTadhg <eoghan.tadhg@gmail.com>
2016-06-04 12:06:08 -0700

    Added required user states to utility items. Placed utility list item content in correct place. Created new JS file: events, which is an assortment of global events triggered by data-event attribute.

---
+++ client/js/actions.js
@@ -0,0 +1,15 @@
+//Actions are initiated by 'data-action' attributes on elements.
+//By applying specific 'data-action' attributes to elements, global app events may occur.
+
+Template.body.events({
+    //Sign Out [data-action=signout]
+    'click [data-action="signout"]': function (event) {
+        //Need event to close asides
+        event.preventDefault(); event.stopPropagation();
+        Meteor.logout(function (error) {
+            if (error instanceof Error)
+                console.error(error);
+        })
+    }
+    
+});
--- packages/thriver-navigation/lib/utility.js
+++ packages/thriver-navigation/lib/utility.js
@@ -1,5 +1,123 @@
+// Populate Profile tab under Account Overview
+Template.utility.helpers({
+    items: [{ 
+        //Notifications
+        //title: 'Notifications', //Readable Title
+        icon : 'bell', //Icon class to be applied (see icons.less)
+        //iconAfter: true, //default is false. Will place the icon :after instead of :before
+        type: 'sidebar', //Sets Link type. (Accepts: sidebar, external, null) *Required
+        sidebar: 'notifications', //which sidebar to activate. *required if type=sidebar (Should be same as sidebars.item.id).
+        //position: 'right' //default is left (accepts: 'left', 'right')
+        user: 'active', //Set If item should only be active when logged in/out (accepts: 'active','inactive')
+        alerts: true //Set to true to add notification element
+        //url: 'wcasa.org', //*required if type=external
+        //more: 'getHelp' //If list item has additional elements within. Get the template
+    },{ 
+        //Accounts
+        icon : 'caret-right',
+        iconAfter: true,
+        type: 'sidebar',
+        sidebar: 'accounts',
+        user: 'active'
+    },{ 
+        //Sign In
+        title: 'Sign In',
+        icon : 'Caret-right',
+        iconAfter: true,
+        type: 'sidebar',
+        sidebar: 'signin',
+        user: 'inactive'
+    },{  
+        //Register
+        title: 'Create an Account',
+        icon : 'caret-right',
+        iconAfter: true,
+        type: 'sidebar',
+        sidebar: 'register',
+        user: 'inactive'
+    },{ 
+        //Sign Out
+        title: 'Sign Out',
+        icon : 'caret-right',
+        iconAfter: true,
+        type: 'null',
+        user: 'active',
+        action: 'signout'
+    },{ 
+        //Language
+        icon : 'flag',
+        type: 'sidebar',
+        sidebar: 'lang',
+        position : 'right',
+        user: 'public'
+    },{ 
+        //Get Help
+        title: 'Get Help',
+        type: 'link',
+        url: '#providers',
+        icon : 'important',
+        position : 'right',
+        more: 'help',
+        user: 'public'
+    },{ 
+        //Donate
+        title: 'Donate',
+        icon : 'heart',
+        type: 'sidebar',
+        sidebar: 'donate',
+        position : 'right',
+        user: 'public'
+    },{
+        //Learning Center
+        title: 'Learning Center',
+        icon : 'institution',
+        type: 'sidebar',
+        sidebar: 'resources',
+        position : 'right',
+        user: 'public'
+    },{ 
+        //News
+        title: 'News',
+        icon : 'news',
+        type: 'sidebar',
+        sidebar: 'news',
+        position : 'right',
+        user: 'public'
+    },{ 
+        //Twitter
+        title: 'Twitter',
+        icon : 'twitter',
+        type: 'sidebar',
+        sidebar: 'twitter',
+        position : 'right',
+        user: 'public'
+    },{ 
+        //Youtube
+        icon : 'Youtube',
+        type: 'link',
+        url: 'https://www.youtube.com/user/WCASAVPCC',
+        position : 'right',
+        target: '_blank',
+        user: 'public'
+    },{ 
+        //Facebook
+        icon : 'Facebook',
+        type: 'link',
+        url: 'https://www.facebook.com/wcasa',
+        position : 'right',
+        target: '_blank',
+        user: 'public'
+    }]
+});
 
-
+Template.utilityItem.helpers({
+    name: function () {
+        var user = Meteor.user();
+        if (user && user.profile)
+            return user.profile.firstname + ' ' + user.profile.lastname;
+        return '';
+    }
+});
 /**
  * Remove all open sidebars
  * @method
