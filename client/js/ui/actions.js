61774546e5ed19d66d3e8bb13a874144aaa0ad79
EoghanTadhg <eoghan.tadhg@gmail.com>
2016-06-04 12:54:58 -0700

    Added correct scope to function in events.js

--- client/js/ui/actions.js
+++ client/js/ui/actions.js
@@ -1,10 +1,9 @@
 //Actions are initiated by 'data-action' attributes on elements.
 //By applying specific 'data-action' attributes to elements, global app events may occur.
-
 Template.body.events({
     //Sign Out [data-action=signout]
     'click [data-action="signout"]': function (event) {
-        clearCanvas();
+        c.clearCanvas();
         event.preventDefault(); event.stopPropagation();
         Meteor.logout(function (error) {
             if (error instanceof Error)
