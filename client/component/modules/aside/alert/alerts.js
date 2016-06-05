e7ecd87225e734e88702a3e752429b902cf8418a
EoghanTadhg <eoghan.tadhg@gmail.com>
2016-06-04 20:28:30 -0700

    Made alerts template reactive.

--- client/component/modules/aside/alert/alerts.js
+++ client/component/modules/aside/alert/alerts.js
@@ -1,8 +1,18 @@
-if (Meteor.isClient) {
-    Template.alerts.events({
-        'click span.closeAlert': function (event) {
-            document.getElementById('alert').classList.remove("active");
-        }
-    });
-}
+Template.alerts.helpers({
+    alerts: [{
+        message: "If this is an emergency, please dial 911 or use our Service Provider Locator Tool to get help right away.",
+        class: "important"
+    },{
+        message: "You have been logged-out as john.doe@gmail.com. See you later John!",
+        class: "default"
+    },{
+        message: "Welcome John! You are now logged in as john.doe@gmail.com.",
+        class: "positive"
+    }]
+});
 
+Template.alerts.events({
+    'click [data-action=closeAlert]': function (event) {
+        document.querySelector('.alert[aria-hidden=false]').setAttribute("aria-hidden", "true");
+    }
+});
