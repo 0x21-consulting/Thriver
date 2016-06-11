f2b937a55b5d61849da0fef76111ff120c9bdd5d
EoghanTadhg <eoghan.tadhg@gmail.com>
2016-06-10 22:05:57 -0700

    Made masthead reactive and utilized tabs template.

--- packages/thriver-masthead/lib/masthead.js
+++ packages/thriver-masthead/lib/masthead.js
@@ -1,19 +1,30 @@
+// Populate Profile tab under Account Overview
+Template.masthead.helpers({
+    items: [{
+        //Need to find away to get background images in here
+        tabs: [{ //If sidebar has tabs: use this property
+                title: 'April is Sexual Assault Awareness Month',
+                id: 'mastheadSlideA', //These are for aria-controls
+                template : 'slideA' //This could really just be an editable content area instead of unique templates
+            },{
+                title: 'Join the WCASA team in making a big difference',
+                id: 'mastheadSlideB',
+                template : 'slideB'
+            },{
+                title: 'Training Institute Conference',
+                id: 'mastheadSlideC',
+                template: 'slideC'
+            }
+        ]
+    }]
+})
+
 Template.masthead.events({
-    // Switch tabs
-    'click ul.mastheadTabs > li': function (event) {
-        var index = $(event.target).index() + 1;
-        // Set the active tab
-        $('ul.mastheadTabs > li').removeClass('active');
-        $(event.target).addClass('active');
-        
-        // Set the active content
-        $('ul.mastheadContent > li').removeClass('active');
-        $('ul.mastheadContent > li:nth-child(' + index + ')').addClass('active');
-    },
     'click section.masthead .arrow': function (event) {
         offset = $('[id="what-we-do"]').offset().top - 125;
         $('body').animate({ scrollTop: offset }, 500);
     },
+    //These are temporary for demo purposes: need to find a way to quickly link with rich content
     'click section.masthead .jobsOpen': function (event) {
         offset = $('[id="get-involved"]').offset().top - 125;
         $('body').animate({ scrollTop: offset }, 1500);
@@ -24,28 +35,23 @@ Template.masthead.events({
     }
 });
 
-
 Template.masthead.onRendered(function () {
+    //Auto Rotate function
     function transitionSlider(){
-        $( "ul.mastheadTabs > li" ).each(function() {
+        $( "#masthead menu.tabs > li" ).each(function() {
             if($(this).hasClass( "active" ) && $(this).is(':not(:last-child)')){
-                $(this).next().trigger( "click" ); 
-                return false; 
+                $(this).next().trigger( "click" );
+                return false;
             }
             if($(this).hasClass( "active" ) && $(this).is(':last-child')){
                 $('ul.mastheadTabs > li:first-child').trigger( "click" );
                 return false;
             }
-        });    
+        });
     }
-
     var sliderTimer = setInterval(transitionSlider, 9000);
-    $("ul.mastheadTabs > li").click(function() {
+    $("#masthead menu.tabs > li").click(function() {
         clearInterval(sliderTimer);
         sliderTimer = setInterval(transitionSlider, 9000);
     });
 });
-
-
-
-
