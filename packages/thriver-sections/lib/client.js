379dc58c31c640f099aa3b425dfc9aaed9d6f854
EoghanTadhg <eoghan.tadhg@gmail.com>
2016-06-19 09:58:33 -0700

    Catching up.

--- packages/thriver-sections/lib/client.js
+++ packages/thriver-sections/lib/client.js
@@ -21,28 +21,28 @@ Template.body.helpers({
  */
 Template.registerHelper('anchor', function (name) {
     var removeName;
-    
+
     // Name must exist and be a string
     check(name, Spacebars.kw);
-    
+
     // We're expecting to only build anchors for elements that actually have names
     if (!name || !name.hash || !name.hash.name)
         return '';
-    
+
     // Mutual Suspicion
     name = '' + name.hash.name;
-    
+
     // Is the name an empty string?
     removeName = !name.length;
-    
+
     // Make all lower case, then replace spaces with hyphens
     name = name.toLowerCase().trim().replace(/ /g, '-').
-    
+
     // anchors can't begin with numbers or hyphens
     replace(/^[\d-]*/g, '');
-    
+
     if (removeName || name.length > 0)
         return name;
-        
+
     return '';  // otherwise return an empty string
 });
