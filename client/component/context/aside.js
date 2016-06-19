e423d0fdcb2e819f921c2edd3d65b190ac5cbd0d
EoghanTadhg <eoghan.tadhg@gmail.com>
2016-06-18 17:11:44 -0700

    Flex boxs added to list items in tabs.

--- client/component/context/aside.js
+++ client/component/context/aside.js
@@ -2,9 +2,10 @@
 Template.aside.helpers({
     items: [{
         title: 'Account Details',
-        id: 'accounts', //Sets the ID of the sidebar which gets targeted by utility nav items 
+        id: 'accounts', //Sets the ID of the sidebar which gets targeted by utility nav items
         width: 656, //Sets the sidebar width & body class
         position:'left', //Which Direction the sidebar appears from
+        class: 'top', //Style. accepts 'left' and 'top'
         tabs: [{ //If sidebar has tabs: use this property
                 title: 'Profile',
                 icon : 'user',
@@ -27,36 +28,37 @@ Template.aside.helpers({
                 template: 'requests'
             }
         ]
-    },{ 
+    },{
         title: 'Notifications',
-        id: 'notifications', 
-        width: 356, 
+        id: 'notifications',
+        width: 356,
         position:'left',
         template: 'notifications'
-    },{ 
+    },{
         title: 'Account Sign in',
-        id: 'signin', 
-        width: 356, 
+        id: 'signin',
+        width: 356,
         position:'left',
         template: 'signin'
-    },{ 
+    },{
         title: 'Create an Account',
-        id: 'register', 
-        width: 356, 
+        id: 'register',
+        width: 356,
         position:'left',
-        template: 'register' 
-    },{ 
+        template: 'register'
+    },{
         title: 'Twitter',
-        id: 'twitter', 
-        width: 356, 
+        id: 'twitter',
+        width: 356,
         position:'right',
-        template: 'twitter' 
-    },{ 
+        template: 'twitter'
+    },{
         title: 'Learning Center',
-        id: 'resources', 
-        width: 700, 
+        id: 'resources',
+        width: 700,
         position:'right',
         filter: 'true',
+        class: 'top',
         filterId: 'searchResources',
         tabs: [{ //If sidebar has tabs: use this property
                 title: 'Library',
@@ -76,13 +78,14 @@ Template.aside.helpers({
                 template: 'webinars'
             }
         ]
-    },{ 
+    },{
         title: 'Newsroom',
-        id: 'news', 
-        width: 1100, 
+        id: 'news',
+        width: 1100,
         position:'right',
         filter: 'true',
         filterId: 'searchNews',
+        class: 'top',
         tabs: [{ //If sidebar has tabs: use this property
                 title: 'In the News',
                 id: 'in-the-news',
@@ -105,17 +108,17 @@ Template.aside.helpers({
                 template: 'pressMediaKits'
             }
         ]
-    },{ 
+    },{
         title: 'Donate to WCASA',
-        id: 'donate', 
-        width: 656, 
+        id: 'donate',
+        width: 656,
         position:'right',
-        template: 'donate' 
-    },{ 
+        template: 'donate'
+    },{
         title: 'Language Select',
-        id: 'lang', 
-        width: 356, 
+        id: 'lang',
+        width: 356,
         position:'right',
-        template: 'languageSelect' 
+        template: 'languageSelect'
         }]
 });
