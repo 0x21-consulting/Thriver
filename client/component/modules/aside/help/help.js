788a97ed2dc8c2752a43f1f7f478707de9a028b6
EoghanTadhg <eoghan.tadhg@gmail.com>
2016-06-21 23:48:11 -0700

    Completed get help popup.

--- client/component/modules/aside/help/help.js
+++ client/component/modules/aside/help/help.js
@@ -1,3 +1,3 @@
 Template.help.helpers({
-    content: "<h2>National Emergency Hotlines</h2><dl><dt>RAINN (Rape, Abuse, Incest National Network)</dt><dd>800.656.HOPE (4673)</dd><dt>National Domestic Violence Hotline</dt><dd>800.799.SAFE (7233)<br>800.787.3224 TDD</dd></dl><a href='#service-providers'>Find a Service Provider</a>"
+    content: "<h2>National Emergency Hotlines</h2><dl><dt>RAINN (Rape, Abuse, Incest National Network)</dt><dd>800.656.HOPE (4673)</dd><dt>National Domestic Violence Hotline</dt><dd>800.799.SAFE (7233)<br>800.787.3224 TDD</dd></dl><a class='mobile' href='#service-providers'>Find a Service Provider</a>"
 });
