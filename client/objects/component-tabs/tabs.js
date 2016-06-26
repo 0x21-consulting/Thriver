5a678e1c534338f8ea21d3499cd0ff4a17c4e071
EoghanTadhg <eoghan.tadhg@gmail.com>
2016-06-25 17:31:36 -0700

    Localized scope of tabs function.

--- client/objects/component-tabs/tabs.js
+++ client/objects/component-tabs/tabs.js
@@ -5,15 +5,24 @@ Meteor.tabsFunctions = {
 	    var toggle = document.querySelectorAll('[aria-controls][data-toggle=tabs]');
 	    var content = document.querySelectorAll('div.tabs > [aria-hidden]');
 	    if(event.target.hasAttribute('aria-controls') && event.target.getAttribute('aria-expanded') == 'false'){
-			document.body.classList.add('tab-open');
-	        for (var i = 0, e; e = toggle[i]; i++) { h.active(e,false); }
-	        h.active(event.target, true);
-	        for (var i = 0, e; e = content[i]; i++) {
-	            h.hidden(e,true); //Clear all active content
+			var thisToggleMenu = event.target.parentNode.parentNode;
+			var thisToggleMenuButtons = thisToggleMenu.querySelectorAll('[aria-controls][data-toggle=tabs]');
+			var thisTabsContent = thisToggleMenu.parentNode.querySelector('div.tabs');
+			var thisTabsContentElement = thisTabsContent.querySelectorAll('[aria-hidden]');
+			for (var i = 0, e; e = thisToggleMenuButtons[i]; i++) {
+				h.active(e,false);
+			}
+			h.active(event.target, true);
+			for (var i = 0, e; e = thisTabsContentElement[i]; i++) {
+				h.hidden(e, true);
 	            if ('#' + e.getAttribute('id') == event.target.getAttribute('aria-controls')){ //If Sidebar ID matches toggles' data-sidebar
 	                h.hidden(e,false); //Add active class to given sidebar
 	            }
-	        }
+			}
+			/*
+	        for (var i = 0, e; e = content[i]; i++) {
+	            h.hidden(e,true); //Clear all active content
+	        }*/
 	    }
 	}
 }
