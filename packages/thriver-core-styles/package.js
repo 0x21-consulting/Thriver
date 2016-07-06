35be6b44ef622d35b45f5182b7bcd61d8502fef3
EoghanTadhg <eoghan.tadhg@gmail.com>
2016-07-06 06:53:51 -0500

    Moved less styles into client folder.

--- packages/thriver-core-styles/package.js
+++ packages/thriver-core-styles/package.js
@@ -8,23 +8,23 @@ Package.describe({
 
 Package.onUse(function(api) {
     api.versionsFrom('METEOR@1.0');
-    
+
     var packages = [
       'less@=2.6.0',    // LessCSS Support
       'fortawesome:fontawesome'
     ];
-    
+
     api.use  (packages);
     api.imply(packages);
     api.use(['thriver:core']);
-    
+
     api.addAssets([
-        
+
     ], ['client']);
-    
-    api.addFiles([
+
+    /*api.addFiles([
         'lib/index.less'
-    ], ['client']);
+    ], ['client']);*/
 });
 
 Package.onTest(function(api) {
