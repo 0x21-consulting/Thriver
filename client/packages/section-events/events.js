acc305160f426c87d4600d6cb59dda4c9e28c310
EoghanTadhg <eoghan.tadhg@gmail.com>
2016-07-12 02:57:06 -0500

    Completed mobile/desktop events section.

--- client/packages/section-events/events.js
+++ client/packages/section-events/events.js
@@ -19,13 +19,13 @@ lastDate = function (specificMonth) {
     if (isNaN(specificMonth) || specificMonth < 0 || specificMonth > 11)
         // if invalid month number, just use current month
         specificMonth = month.get();
-    
+
     // if February
     if (specificMonth === 1) {
         // If the year is divisible by both 4 and 100, but not 400,
         // then it's a leap year
         if (year.get() % 4 === 0)
-            if ( (year.get() % 100) === 0 && 
+            if ( (year.get() % 100) === 0 &&
                  (year.get() % 400) !== 0? false : true)
                     return 29;
         return 28;
@@ -33,7 +33,7 @@ lastDate = function (specificMonth) {
     // There can't be more than 30 days in April, June, Sept., or Nov.
     if (![3,5,8,10].every(function (val) { return specificMonth !== val; }))
         return 30;
-    
+
     // All other months
     return 31;
 },
@@ -45,8 +45,8 @@ lastDate = function (specificMonth) {
  */
 getLastMonth = function () {
     var lastMonth = month.get() - 1;
-    
-    return lastMonth < 0 
+
+    return lastMonth < 0
         ? (12 - Math.abs(lastMonth) % 12) : (lastMonth  % 12);
 },
 
@@ -63,8 +63,8 @@ Template.events.helpers({
      */
     lastMonth: function () {
         var lastMonth = month.get() - 1;
-        
-        return months[ getLastMonth() ] + ' ' + 
+
+        return months[ getLastMonth() ] + ' ' +
             ( lastMonth < 0 ? (year.get() - 1) : year.get() );
     },
     /**
@@ -74,7 +74,7 @@ Template.events.helpers({
      */
     nextMonth: function () {
         var nextMonth = month.get() + 1;
-        
+
         return months[ nextMonth % 12 ] + ' ' +
             (          nextMonth > 11 ?
             (year.get() + 1) : year.get() );
@@ -107,18 +107,18 @@ Template.calendar.helpers({
     week: function () {
         var firstDay = new Date(year.get(), month.get()).getDay(),
             total    = lastDate(),
-            weeks    = [], 
+            weeks    = [],
             week, day, i, j, count = 0,
-            
+
             // Last day of last month
             lastMonth = lastDate( getLastMonth() ),
-            
+
             // Today
             today = new Date(), thisWeek, thisDay,
-            
+
             // All events this month
             currentEvents = {};
-        
+
         // Get all events and organize them in an easily-accessible way
         Events.find({
             start: { $gt: new Date( year.get(), month.get() ) },
@@ -126,14 +126,14 @@ Template.calendar.helpers({
         }).forEach(function (event) {
             // If there's no start date, do nothing
             if (! (event.start instanceof Date)) return;
-            
+
             var start = event.start.getDate(),
                 total = 1, i;
-            
+
             // If there's an end date, calculate total number of days
             if (event.end instanceof Date)
                 total = event.end.getDate() - start + 1;
-            
+
             // For each day, add event info
             for (i = 0; i < total; ++i) {
                 // If the date doesn't already exist, add it
@@ -143,25 +143,25 @@ Template.calendar.helpers({
                 currentEvents[ start + i ].push(event);
             }
         });
-            
+
         console.debug('Events\n',
             'Greater than', new Date( year.get(), month.get() ), '\n',
             'Less than', new Date( year.get(), month.get(), total ), '\n',
             currentEvents
         );
-        
+
         // If today is in the current month and year
-        if (today.getMonth() === month.get() && 
+        if (today.getMonth() === month.get() &&
             today.getFullYear() === year.get()) {
                 thisDay  = today.getDate();
                 thisWeek = today.getDate() - today.getDay();
             }
-        
+
         // Create weeks until we reach last day of the month
         do {
             // Create week
             week = [];
-            
+
             // Create seven days
             for (i = 0; i < 7; ++i) {
                 day = {};
@@ -187,38 +187,38 @@ Template.calendar.helpers({
                     day.date = count;
                     ++count;
                 }
-                
+
                 // If today or this week, add special styles
                 if (day.date === thisWeek)
                     day.currentWeekStart = 'currentWeekStart';
                 if (day.date === thisDay)
                     day.today = 'today';
-                
+
                 // If there are events this day
                 if (currentEvents[count] instanceof Array) {
                     day.hasEvent = 'hasEvent';
-                    
+
                     // If the day is in the past
                     if (count < thisDay)
                         day.past = 'past';
-                    
+
                     // Hyperlink first event
                     day.href = currentEvents[count][0]._id;
-                    
+
                     // Add event details
                     // (and convert to array)
                     day.currentEvents = currentEvents[count];
                     console.info('Day', count, day); //debug
                 }
-                
+
                 // Add day
                 week.push(day);
             }
-            
+
             // Add week
             weeks.push(week);
         } while (count <= total);
-        
+
         // Return weeks
         return weeks;
     }
@@ -239,12 +239,12 @@ Template.events.events({
      */
     'click .scroll-prev-month, click .prevMonth': function (event) {
         if ( !(event instanceof $.Event) ) return;
-        
+
         var lastMonth = month.get() - 1;
-        
+
         if (lastMonth < 0)
             year.set( year.get() - 1 );
-        
+
         month.set( getLastMonth() );
     },
     /**
@@ -254,15 +254,15 @@ Template.events.events({
      */
     'click .scroll-next-month, click .nextMonth': function (event) {
         if ( !(event instanceof $.Event) ) return;
-        
+
         var nextMonth = month.get() + 1;
-        
+
         if (nextMonth > 11)
             year.set( year.get() + 1);
-        
+
         month.set(nextMonth % 12);
     },
-    
+
     // Eoghan's stuff
     'click .sliderPrev': function (event) {
         if (prevPosition >= 0){
@@ -282,12 +282,14 @@ Template.events.events({
         document.querySelector('.slides').style.webkitTransform = "translate(-" + event.target.value + "00% ,0px)";
         nextPosition = Number(event.target.value) + 1;
         prevPosition = Number(event.target.value) - 1;
+        calMobileEvent();
     },
     'click a.eventDate': function (event) {
         event.preventDefault();
         document.querySelector('.slides').style.webkitTransform = "translate(-" + event.target.dataset.value + "00% ,0px)";
         nextPosition = Number(event.target.dataset.value) + 1;
         prevPosition = Number(event.target.dataset.value) - 1;
+        calMobileEvent();
     },
     'click .unregister a': function (event) {
         event.preventDefault();
@@ -310,8 +312,19 @@ Template.events.events({
     },
     'click .eventSlide .actions .details': function (event) {
         $(event.currentTarget).parent().parent().parent().parent().addClass('extendedContent');
+    },
+    'click .back-to-events': function (event) {
+        calMobileEvent();
     }
 });
 
 
 
+function calMobileEvent(){
+    if (!document.body.classList.contains('active-event')){
+        document.body.classList.add('active-event');
+        document.querySelector('.eventSlide .actions .details').click();
+    } else{
+        document.body.classList.remove('active-event');
+    }
+}
