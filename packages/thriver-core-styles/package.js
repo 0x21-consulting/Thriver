a9dbdaff36f776b60539ee5745e22462cce5513f
Micah Henning <me@micahhenning.com>
2016-10-24 20:16:30 -0500

    Updated Core Styles package to meet lint guidelines.

--- packages/thriver-core-styles/package.js
+++ packages/thriver-core-styles/package.js
@@ -1,50 +1,50 @@
 Package.describe({
-    name: 'thriver:core-styles',
-    version: '0.0.1-teal',
-    summary: 'Base styles for the Thriver CMS',
-    git: 'https://github.com/enove/Thriver.git',
-    documentation: 'README.md'
+  name: 'thriver:core-styles',
+  version: '0.0.1-teal',
+  summary: 'Base styles for the Thriver CMS',
+  git: 'https://github.com/enove/Thriver.git',
+  documentation: 'README.md',
 });
 
-Package.onUse(function (api) {
-    api.versionsFrom('METEOR@1.0');
+Package.onUse((api) => {
+  api.versionsFrom('METEOR@1.0');
 
-    var packages = [
-      'less@=2.6.0',    // LessCSS Support
-      'fortawesome:fontawesome'
-    ];
+  const packages = [
+    'less@=2.6.0',    // LessCSS Support
+    'fortawesome:fontawesome',
+  ];
 
-    api.use  (packages);
-    api.imply(packages);
-    api.use(['thriver:core']);
+  api.use(packages);
+  api.imply(packages);
+  api.use(['thriver:core']);
 
-    api.addAssets([
+  api.addAssets([
 
-    ], ['client']);
+  ], ['client']);
 
-    api.addFiles([
-        'lib/index.less',
-        
-        // Styles
-        'lib/vendor/details-shim.less',
-        'lib/styles/animate.less',
-        'lib/styles/buttons.less',
-        'lib/styles/context.less',
-        'lib/styles/fonts.less',
-        'lib/styles/forms.less',
-        'lib/styles/helpers.less',
-        'lib/styles/icons.less',
-        'lib/styles/print.less',
-        'lib/styles/reset.less',
-        'lib/styles/scroll.less',
-        'lib/styles/tables.less',
-        'lib/styles/type.less'
-    ], ['client']);
+  api.addFiles([
+    'lib/index.less',
+
+    // Styles
+    'lib/vendor/details-shim.less',
+    'lib/styles/animate.less',
+    'lib/styles/buttons.less',
+    'lib/styles/context.less',
+    'lib/styles/fonts.less',
+    'lib/styles/forms.less',
+    'lib/styles/helpers.less',
+    'lib/styles/icons.less',
+    'lib/styles/print.less',
+    'lib/styles/reset.less',
+    'lib/styles/scroll.less',
+    'lib/styles/tables.less',
+    'lib/styles/type.less',
+  ], ['client']);
 });
 
-Package.onTest(function (api) {
-    api.use('ecmascript');
-    api.use('tinytest');
-    api.use('thriver:core-styles');
-    api.mainModule('core-styles-tests.js');
+Package.onTest((api) => {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('thriver:core-styles');
+  // api.mainModule('core-styles-tests.js');
 });
