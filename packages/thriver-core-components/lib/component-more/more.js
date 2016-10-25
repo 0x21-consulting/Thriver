a7bbd161b42a01c5143380f48d3e430b686fbd90
Micah Henning <me@micahhenning.com>
2016-10-24 20:14:34 -0500

    Updated Core Components package to meet lint guidelines.

--- packages/thriver-core-components/lib/component-more/more.js
+++ packages/thriver-core-components/lib/component-more/more.js
@@ -1,31 +1,27 @@
-//more.js controls display events for elements containing details/more. Via hover or click.
-var more = {
-    toggleMore : function(){
-        var parent = Thriver.util.findAncestor(event.target, 'more');
-        var e = parent.getElementsByTagName('figure')[0];
-        if (event.target.getAttribute('aria-expanded') == 'false'){
-            Thriver.util.makeActive(event.target,true);
-            Thriver.util.hide(e,false);
-        } else{
-            event.target.setAttribute('aria-expanded', 'false');
-            Thriver.util.makeActive(event.target,false);
-            Thriver.util.hide(e,true);
-        }
+// more.js controls display events for elements containing details/more. Via hover or click.
+const more = {
+  toggleMore: () => {
+    const parent = Thriver.util.findAncestor(event.target, 'more');
+    const e = parent.getElementsByTagName('figure')[0];
+
+    if (event.target.getAttribute('aria-expanded') === 'false') {
+      Thriver.util.makeActive(event.target, true);
+      Thriver.util.hide(e, false);
+    } else {
+      event.target.setAttribute('aria-expanded', 'false');
+      Thriver.util.makeActive(event.target, false);
+      Thriver.util.hide(e, true);
     }
-}
-//Define usage
-//m = Meteor.moreFunctions;
+  },
+};
 
 Template.body.events({
-    //'More' Hovers
-    'mouseenter li.more > a': function (event) {
-        if (window.innerWidth > 767) {
-            more.toggleMore();
-        }
-    },
-    'mouseleave li.more > a': function (event) {
-        if (window.innerWidth > 767) {
-            more.toggleMore();
-        }
-     }
+  // 'More' Hovers
+  'mouseenter li.more > a': () => {
+    if (window.innerWidth > 767) more.toggleMore();
+  },
+
+  'mouseleave li.more > a': () => {
+    if (window.innerWidth > 767) more.toggleMore();
+  },
 });
