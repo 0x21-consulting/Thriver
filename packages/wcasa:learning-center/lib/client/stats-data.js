3c922b6397428e65af098ab1784faaf344ea940e
Micah Henning <me@micahhenning.com>
2016-10-29 14:37:49 -0500

    Updated Learning Center package to meet lint guidelines.

--- packages/wcasa:learning-center/lib/client/stats-data.js
+++ packages/wcasa:learning-center/lib/client/stats-data.js
@@ -1,36 +1,37 @@
+// TODO(micchickenburger): Remove dummy data and connect to db
 Template.statsData.helpers({
-    lists: [{
-	    type: 'article', //accepts: generic, details, article
-	    itemType: 'category',
-	    paginate: true, //Default is false
-	    perPage: 10, //if paginate:true, how many before paginate (Not currently functioning)
-	    style: 'stripes',
-	    categories: [{
-	        title: 'Summer Webinars',
-	        id: 'summerWebinars',
-	        items: [{
-	            title: 'One Webinar',
-	            date: new Date('2016-01-01'), //This is temporary
-	            friendlyDate: '02/11/29',
-	            content: 'lorem ipsum.',
-	            url: 'http://google.com'
-	        },{
-	            title: 'One Webinar',
-	            date: new Date('2016-01-01'), //This is temporary
-	            friendlyDate: '02/11/29',
-	            content: 'lorem ipsum.',
-	            url: 'http://google.com'
-	        }]
-	    },{
-	        title: 'Summer Webinars',
-	        id: 'summerWebinars',
-	        items: [{
-	            title: 'One Webinar',
-	            date: new Date('2016-01-01'), //This is temporary
-	            friendlyDate: '02/11/29',
-	            content: 'lorem ipsum.',
-	            url: 'http://google.com'
-	        }]
-	    }]
-	}]
+  lists: [{
+    type: 'article', // accepts: generic, details, article
+    itemType: 'category',
+    paginate: true, // Default is false
+    perPage: 10, // if paginate:true, how many before paginate (Not currently functioning)
+    style: 'stripes',
+    categories: [{
+      title: 'Summer Webinars',
+      id: 'summerWebinars',
+      items: [{
+        title: 'One Webinar',
+        date: new Date('2016-01-01'), // This is temporary
+        friendlyDate: '02/11/29',
+        content: 'lorem ipsum.',
+        url: 'http://google.com',
+      }, {
+        title: 'One Webinar',
+        date: new Date('2016-01-01'), // This is temporary
+        friendlyDate: '02/11/29',
+        content: 'lorem ipsum.',
+        url: 'http://google.com',
+      }],
+    }, {
+      title: 'Summer Webinars',
+      id: 'summerWebinars',
+      items: [{
+        title: 'One Webinar',
+        date: new Date('2016-01-01'), // This is temporary
+        friendlyDate: '02/11/29',
+        content: 'lorem ipsum.',
+        url: 'http://google.com',
+      }],
+    }],
+  }],
 });
