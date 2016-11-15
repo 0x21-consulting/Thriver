4bdfcbb0daaf0f4e3bbda79de650790ae453f00b
Micah Henning <me@micahhenning.com>
2016-11-15 00:32:58 -0600

    Added administrative interface to view feedback.

--- packages/wcasa:feedback/lib/server.js
+++ packages/wcasa:feedback/lib/server.js
@@ -2,14 +2,13 @@
  * @summary Publish feedback to admin interface
  * @method
  */
-Meteor.publish('feedback', () => {
-  // Check authorization
-  if (!Meteor.userId() || !Meteor.user().admin) {
-    throw new Meteor.Error('not-authorized');
+Meteor.publish('feedback', function () {
+  // Publish
+  if (this.userId && Meteor.users.findOne({ _id: this.userId }).admin) {
+    return Thriver.feedback.collection.find();
   }
 
-  // Publish
-  return Thriver.feedback.collection.find();
+  return [];
 });
 
 Meteor.methods({
