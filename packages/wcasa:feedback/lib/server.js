d8135485b4b7784affa873cca4649da16f3a9c83
Micah Henning <me@micahhenning.com>
2016-11-14 23:21:31 -0600

    Created feedback schema, collection, namespace, publish method, and add method. Allows for submitting comments and pairing them with an element on the page.

---
+++ packages/wcasa:feedback/lib/server.js
@@ -0,0 +1,39 @@
+/**
+ * @summary Publish feedback to admin interface
+ * @method
+ */
+Meteor.publish('feedback', () => {
+  // Check authorization
+  if (!Meteor.userId() || !Meteor.user().admin) {
+    throw new Meteor.Error('not-authorized');
+  }
+
+  // Publish
+  return Thriver.feedback.collection.find();
+});
+
+Meteor.methods({
+  /**
+   * @summary Add feedback
+   * @method
+   *   @param {string} path - Element path in markup
+   *   @param {string} comments - User comments
+   */
+  addFeedback: (path, comments) => {
+    // Check authorization
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    // Parameter checks
+    check(path, Match.Maybe(String));
+    check(comments, String);
+
+    // Add feedback
+    Thriver.feedback.collection.insert({
+      path,
+      comments,
+      userId: Meteor.userId(),
+    });
+  },
+});
