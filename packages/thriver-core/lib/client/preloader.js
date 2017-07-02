0fdc9ef188999051d86b91ba4ff4e09b479db41b
EoghanTadhg <eoghan.tadhg@gmail.com>
2017-07-02 15:48:41 -0700

    Added site preloader. Regarding #283.

---
+++ packages/thriver-core/lib/client/preloader.js
@@ -0,0 +1,8 @@
+/**
+ * @summary Handle form submission
+ * @method
+ */
+
+Template.canvas.onRendered( function() {
+  window.setTimeout(function(){$("#app-preloader").addClass("loading");}, 100);
+});
