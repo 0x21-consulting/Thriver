537bf3fe51dd9b80fdfac2eaef68c3b2aeb6ddfb
Micah Henning <micah@perceptive.io>
2018-11-11 15:19:29 -0600

    Fixed template bug if no donations or payments exist

--- views/accounts/receipts.js
+++ views/accounts/receipts.js
@@ -7,15 +7,27 @@ Meteor.subscribe('donations');
 Meteor.subscribe('purchases');
 
 Template.receiptsList.helpers({
-  donations: () => Meteor.users.findOne({
-    _id: Meteor.userId(),
-    'payments.description': 'WCASA Donation',
-  }).payments,
+  donations: () => {
+    const user = Meteor.users.findOne({
+      _id: Meteor.userId(),
+      'payments.description': 'WCASA Donation',
+    });
+    if (user && user.payments && user.payments.length) {
+      return user.payments;
+    }
+    return undefined;
+  },
 
-  purchases: () => Meteor.users.findOne({
-    _id: Meteor.userId(),
-    'payments.description': { $ne: 'WCASA Donation' },
-  }).payments,
+  purchases: () => {
+    const user = Meteor.users.findOne({
+      _id: Meteor.userId(),
+      'payments.description': { $ne: 'WCASA Donation' },
+    });
+    if (user && user.payments && user.payments.length) {
+      return user.payments;
+    }
+    return undefined;
+  },
 
   timestamp() { return this.created * 1000; },
   timestampUTC() { return new Date(this.created * 1000).toISOString(); },
