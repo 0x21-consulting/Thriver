47a3a878f34509d73dd62db3c1572fceb630bd9e
eoghanTadhg <eoghan.tadhg@gmail.com>
2018-11-25 19:49:06 -0600

    Correct implementation of mapLabel.js.

--- views/resources/resources.js
+++ views/resources/resources.js
@@ -250,19 +250,3 @@ Template.libraryAddForm.helpers({
   library: () => Library.collection,
 });
 
-Template.resourceData.helpers({
-  title: 'Data',
-  items: [{
-    tabs: [{ // If sidebar has tabs: use this property
-      title: 'State',
-      icon: 'user',
-      id: 'resourceDataState',
-      template: 'resourceDataState',
-    }, {
-      title: 'National',
-      icon: 'cal',
-      id: 'resourceDataNational',
-      template: 'resourceDataNational',
-    }],
-  }],
-});
