b0cd8e97af28b320ded92fb75d33aa8e0bbad576
Micah Henning <micah@perceptive.io>
2018-11-26 21:57:21 -0600

    Forgot passphrase passes token to reset passphrase form

--- views/accounts/forgot.js
+++ views/accounts/forgot.js
@@ -1,8 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Accounts } from 'meteor/accounts-base';
+import History from '/views/history/history';
 import Toast from '/views/components/toasts';
 
 import './forgot.html';
+
+const Token = new ReactiveVar('');
 
 Template.forgotPassphrase.events({
   'submit #forgotPassphrase'(event) {
@@ -20,5 +25,13 @@ Template.forgotPassphrase.events({
 
     // Tell user to check email regardless of result
     Toast({ text: 'Please check your email for next steps.' });
+    History.navigate('/');
   },
 });
+
+Accounts.onResetPasswordLink((token) => {
+  Token.set(token);
+  History.navigate('/reset-passphrase');
+});
+
+export default Token;
