b4e9a341c2385eb21dd5d0c5d5bafc239c040f23
Micah Henning <micah@perceptive.io>
2018-12-02 01:01:53 -0600

    Initial markdown editor and styling

--- views/canvas/markdown.js
+++ views/canvas/markdown.js
@@ -31,6 +31,31 @@ Template.markdownEditor.events({
   'keyup .markdown-editor textarea'(event) {
     preview.set(event.target.value);
   },
+
+  /**
+   * Handle switching between edit and preview modes
+   * @param {$.Event} event
+   */
+  'click ul.edit-menu li'(event) {
+    const parent = event.target.parentElement.parentElement
+      .parentElement.querySelector('.markdown-editor');
+    const edit = parent.querySelector('textarea');
+    const prev = parent.querySelector('.preview');
+
+    // Show/hide edit and preview elements
+    if (event.target.classList.contains('edit')) {
+      edit.classList.remove('hide');
+      prev.classList.add('hide');
+    } else {
+      edit.classList.add('hide');
+      prev.classList.remove('hide');
+    }
+
+    // Mark active
+    event.target.parentElement.querySelectorAll('li')
+      .forEach(elem => elem.classList.remove('active'));
+    event.target.classList.add('active');
+  },
 });
 
 Template.markdownEditor.onRendered(() => {
