3aa1c67f39e1790ce871faff00c8fbac75115631
eoghanTadhg <eoghan.tadhg@gmail.com>
2018-12-10 15:08:11 -0600

    Added 301 redirects.

--- logic/routes.js
+++ logic/routes.js
@@ -59,3 +59,28 @@ FlowRouter.route('/*', {
     this.render('canvas');
   },
 });
+
+/**
+ * @summary 301 Page Redirects
+ */
+FlowRouter.route('/pages/Home-Page.php', { action() { this.render('canvas'); History.navigate('/'); } });
+FlowRouter.route('/pages/About-Us.php', { action() { this.render('canvas'); History.navigate('/who-we-are'); } });
+FlowRouter.route('/pages/About-Us_Vision-Mission-&-Philosophy.php', { action() { this.render('canvas'); History.navigate('/who-we-are/vision-mission-philosophies'); } });
+FlowRouter.route('/pages/About-Us_Sexual-Violence.php', { action() { this.render('canvas'); History.navigate('/what-we-do'); } });
+FlowRouter.route('/pages/About-Us_Staff.php', { action() { this.render('canvas'); History.navigate('/who-we-are/wcasa-staff'); } });
+FlowRouter.route('/pages/About-Us_Internships.php', { action() { this.render('canvas'); History.navigate('/get-involved/intern'); } });
+FlowRouter.route('/pages/About-Us_Board.php', { action() { this.render('canvas'); History.navigate('/who-we-are/board-of-directors'); } });
+FlowRouter.route('/pages/Contact.php', { action() { this.render('canvas'); History.navigate('/contact'); } });
+FlowRouter.route('/pages/ALL.php', { action() { this.render('canvas'); History.navigate('/what-we-do/anti-oppression'); } });
+FlowRouter.route('/pages/ALL-RACE-ETHNICITY.php', { action() { this.render('canvas'); History.navigate('/what-we-do/anti-oppression/race-ethnicity'); } });
+FlowRouter.route('/pages/ALL-GENDER-Transgender.php', { action() { this.render('canvas'); History.navigate('/what-we-do/anti-oppression/lgbtq'); } });
+FlowRouter.route('/pages/ALL-SEXUAL_ORIENTATION.php', { action() { this.render('canvas'); History.navigate('/what-we-do/anti-oppression/lgbtq'); } });
+FlowRouter.route('/pages/ALL-SEXUAL_ORIENTATION-LGBTQCommittee.php', { action() { this.render('canvas'); History.navigate('/what-we-do/anti-oppression/lgbtq'); } });
+FlowRouter.route('/pages/ALL-AGE_ABILITY.php', { action() { this.render('canvas'); History.navigate('/what-we-do/anti-oppression/disability'); } });
+FlowRouter.route('/pages/ALL-AGE_ABILITY-Deaf.php', { action() { this.render('canvas'); History.navigate('/what-we-do/anti-oppression/deaf'); } });
+FlowRouter.route('/pages/Data-Wisconsin.php', { action() { this.render('canvas'); History.navigate('/resource-center/data'); } });
+FlowRouter.route('/pages/Data-National.php', { action() { this.render('canvas'); History.navigate('/resource-center/data'); } });
+FlowRouter.route('/pages/Intervention_Advocacy.php', { action() { this.render('canvas'); History.navigate('/what-we-do/advocacy'); } });
+FlowRouter.route('/pages/Intervention_Human-Trafficking.php', { action() { this.render('canvas'); History.navigate('/what-we-do/advocacy/sexual-assault-victim-advocacy-school'); } });
+FlowRouter.route('/pages/Intervention_SurvivorsAndAlliesTaskForce.php', { action() { this.render('canvas'); History.navigate('/what-we-do/survivors-allies'); } });
+FlowRouter.route('/pages/Donate.php', { action() { this.render('canvas'); History.navigate('/donate/'); } });
