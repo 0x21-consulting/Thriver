36602716d3fa24585d72e0cc5da6a5696499d444
eoghanTadhg <eoghan.tadhg@gmail.com>
2018-12-18 07:35:55 -0600

    Update URL path on logo for action-alerts and press-releases.

--- logic/routes.js
+++ logic/routes.js
@@ -13,7 +13,7 @@ FlowRouter.route('/action-alert/:title', {
       logos: [{
         title: 'WCASA',
         src: '/lib/img/wcasa-wisconsin-coalition-against-sexual-assault.svg',
-        url: '/',
+        url: 'https://wcasa.org',
       }],
       friendlyTitle: params.title,
     });
@@ -32,7 +32,7 @@ FlowRouter.route('/press-release/:title', {
       logos: [{
         title: 'WCASA',
         src: '/lib/img/wcasa-wisconsin-coalition-against-sexual-assault.svg',
-        url: '/',
+        url: 'https://wcasa.org',
       }],
       friendlyTitle: params.title,
     });
