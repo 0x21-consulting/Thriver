adbdb9a3fba51e66880a5b1c461d7d9862c6695b
eoghanTadhg <eoghan.tadhg@gmail.com>
2019-01-17 09:40:43 -0600

    Correct spelling of Madison in contact section.

--- views/contact/contact.js
+++ views/contact/contact.js
@@ -25,7 +25,7 @@ Template.contact.helpers({
   imgSrc: '/lib/img/wisconsin-coalition-against-sexual-assault.svg',
   imgAlt: 'Wisconsin Coalition Against Sexual Assault',
   addressL1: '2801 West Beltine Highway, Suite 202',
-  addressL2: 'Madision, Wisconsin 53713',
+  addressL2: 'Madison, Wisconsin 53713',
   mapUrl: 'https://www.google.com/maps/place/2801+W+Beltline+Hwy+%23202,+Madison,+WI+53713/@43.034741,-89.4274008,17z/data=!3m1!4b1!4m2!3m1!1s0x8807ad6daf6daa9d:0x93cfebcf81dadf8a',
 
   // Which contact template to display
